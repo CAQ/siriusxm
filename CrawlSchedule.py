8b99542664d138e7989cea04852fe3bc9c151c4f
CAQ <r+github@caq9.info>
2018-03-13 14:08:37 -0400

    updated gz output and twitter messages.

--- CrawlSchedule.py
+++ CrawlSchedule.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime, urllib2, json
+import datetime, urllib2, json, gzip
 import twitter # https://github.com/bear/python-twitter
 
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
   last_time = None
   last_schedule = None
   try:
-    with open("schedule." + CHANNEL + ".last") as f:
+    with gzip.open("schedule." + CHANNEL + ".last.gz", "rb") as f:
       for line in f:
         (last_time, last_schedule) = line.split("\t")
         last_schedule = json.loads(last_schedule)
@@ -28,9 +28,20 @@ if __name__ == "__main__":
   if l1 is not None:
     del l1["channelMetadataResponse"]["metaData"]["dateTime"]
   if c1 != l1:
-    with open("schedule." + CHANNEL + ".last", "a") as fw:
+    with gzip.open("schedule." + CHANNEL + ".last.gz", "ab") as fw:
       fw.write(now_time + "\t" + content + "\n")
     with open("tokens.txt") as f:
       (ck, cs, atk, ats) = f.readline().strip().split("\t")
+    event = curr_schedule["channelMetadataResponse"]["metaData"]["currentEvent"]
+    message = "[" + now_time + "] Now playing: " + event["song"]["name"] + " by " + event["artists"]["name"]
+    succ = False
+    for sz in ["LARGE", "MEDIUM", "SMALL", "THUMBNAIL"]:
+      for art in event["song"]["creativeArts"]:
+        if not art["encrypted"] and art["type"] == "IMAGE" and art["size"] == sz and len(art["url"]) > 0:
+          message += " " + event["baseUrl"] + art["url"]
+          succ = True
+          break
+      if succ:
+        break
     api = twitter.Api(consumer_key=ck, consumer_secret=cs, access_token_key=atk, access_token_secret=ats)
-    status = api.PostUpdate("[" + str(now) + "] Siruis XM channel " + CHANNEL + " schedule has changed at " + now_time + " UTC.")
+    status = api.PostUpdate(message)
